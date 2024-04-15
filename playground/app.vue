a1d19dbba2261c697af13ee2161ca0c6ca214aea
Rubillex <vadim.gubchenko@cyberia.studio>
2024-04-15 23:34:41 +0700

    v1.0.0

--- playground/app.vue
+++ playground/app.vue
@@ -6,7 +6,7 @@
 
 <script setup lang="ts">
 const {data: testData} = await useAsyncData(
-  () => $rFetch(`/dictionaries?by_name=drivers,transport_vehicles`, {},), {server: false}
+  () => $rubiFetch(`/dictionaries?by_name=drivers,transport_vehicles`, {},), {server: false}
 );
 console.log('testData', testData.value);
 </script>
